2bd8a40a9cba7f7df9fe514395ccff1f7520201c
kyenpya <kyenpyagutap@gmail.com>
2024-03-19 09:55:22 -0700

    Data structures directory and files created

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    copy = my_list[:]
+    if idx >= len(copy) or idx < 0:
+        return copy
+    copy[idx] = element
+    return copy
