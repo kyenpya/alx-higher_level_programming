cb2fcb43bff54d02f95deea0a24ba3db68193ff7
kyenpya <kyenpyagutap@gmail.com>
2024-05-21 05:38:31 -0700

    task 15 again

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 -- lists all records with the same score
 -- in the second_table
 
-SELECT score, COUNT('score')AS NUMBER
+SELECT score, COUNT(*)AS number
 FROM second_table
 GROUP BY score
 ORDER BY score DESC;
