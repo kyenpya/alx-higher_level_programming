d43ee15a7d6dd684263ec25143cc0a208e37d3f4
kyenpya <kyenpyagutap@gmail.com>
2024-05-21 05:43:16 -0700

    task 16 had spelling error. fixed it

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,5 @@
 -- lists all records of the table
 -- second_table where name is not empty
 
-SELECT socre, name FROM second_table
-WHERE name IS NOT NULL
+SELECT score, name FROM second_table WHERE name IS NOT NULL
 ORDER BY score DESC;
