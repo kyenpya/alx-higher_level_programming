8da9f4b73fba64a4aa2a487394fdbe9ad7ba600b
kyenpya <kyenpyagutap@gmail.com>
2024-05-21 05:03:25 -0700

    tasks 15 and 16 corrections

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 -- lists all records with the same score
--- in the second_table TABLE
+-- in the second_table
 
-SELECT score, COUNT(*)AS NUMBER
+SELECT score, COUNT('score')AS NUMBER
 FROM second_table
 GROUP BY score
 ORDER BY score DESC;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- lists all records of the table
--- second_table
+-- second_table where name is not empty
 
 SELECT socre, name FROM second_table
 WHERE name IS NOT NULL
