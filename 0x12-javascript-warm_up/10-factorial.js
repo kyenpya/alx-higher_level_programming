1a80d81d0fa5c0ffa7c08627a3769eee6f52d600
kyenpya <kyenpyagutap@gmail.com>
2024-06-19 08:46:07 -0700

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const args = process.argv.slice(2);
+const number = parseInt(args[0], 10);
+
+console.log(factorial(number));
