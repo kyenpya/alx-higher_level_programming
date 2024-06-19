c361555ec3c5b774e427cd3eb568bd2cc2e840fd
kyenpya <kyenpyagutap@gmail.com>
2024-06-19 09:27:25 -0700

    task 102

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-function callMeMo (x, theFunction) {
+function callMeMoby (x, theFunction) {
   if (x > 0) {
     theFunction();
-    callMeMo(x - 1, theFunction);
+    callMeMoby(x - 1, theFunction);
   }
 }
 
-module.exports.callMeMo = callMeMo;
+module.exports.callMeMoby = callMeMoby;
