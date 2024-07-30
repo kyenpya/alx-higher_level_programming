ddc9723d50beb6608e89435df76de71a2f1bfeff
kyenpya <kyenpyagutap@gmail.com>
2024-07-30 04:13:18 -0700

    tasks 0 and 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-// Reads and prints content from file
 
-const filesys = require('fs');
-filesys.readFile(process.argv[2], 'utf-8',
-  function (err, data) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(data);
-  });
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+});
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,9 +2,8 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
-  function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
+fs.writeFile(process.argv[2], process.argv[3], (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
